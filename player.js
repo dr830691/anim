b3f83830ed5f12316e646676e3271a13caad0e74
dr830691 <52163593+dr830691@users.noreply.github.com>
2020-09-14 13:09:04 +0530

    animation by scrolling is done

---
+++ player.js
@@ -0,0 +1,8 @@
+
+var animation = bodymovin.loadAnimation({
+    container : document.getElementById('anim-home'),
+    renderer : 'svg',
+    loop : true,
+    autoplay : true,
+    path: '17657-birthday-card.json'
+  })
